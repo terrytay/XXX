37b9b926cbd806f062bacde1d088c0d62c657876
Terry Tay <terrytay@Terrys-MacBook-Air.local>
2024-07-20 20:38:20 +0800

    set vercel max duration for GE prices api

--- app/prices/action.ts
+++ app/prices/action.ts
@@ -1,3 +1,5 @@
+export const maxDuration = 12;
+
 export const getPrices = async () => {
   "use server";
 
@@ -5,6 +7,7 @@ export const getPrices = async () => {
     const result = await fetch(process.env.GE_PRICES!, {
       signal: AbortSignal.timeout(10000),
     });
+    console.log(result);
     return await result.json();
   } catch (error) {
     return null;
