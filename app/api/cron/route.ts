1e4862a58ca396b32c90453b76166110528dd0dc
Terry Tay <terrytay@Terrys-MacBook-Air.local>
2024-07-21 16:16:55 +0800

    implement workaround with revalidate = 0

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,14 +1,14 @@
 import { FundPrice } from "@/utils/types/ge";
 import { updatePrices } from "./action";
 
+export const revalidate = 0;
 export async function GET(request: Request) {
   try {
     const result = await fetch(process.env.GE_PRICES!);
     let fundPrices: FundPrice = await result.json();
     fundPrices.fixedId = 1;
     fundPrices.lastUpdated = new Date(Date.now()).toUTCString();
-    await updatePrices(fundPrices);
-    return Response.json({ ok: true });
+    return await updatePrices(fundPrices);
   } catch (error) {
     return Response.json({ ok: false });
   }
