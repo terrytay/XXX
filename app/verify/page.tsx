c93ee66ee5a3fe8a382b2f83507f67cca2c2ce5e
Terry Tay <terrytay@Terrys-MacBook-Air.local>
2024-07-28 18:06:10 +0800

    implement verify user

---
+++ app/verify/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Verify = () => {
+  return (
+    <section className="flex min-h-screen justify-center items-center">
+      <div className="text-gray-400">
+        Account created, please notify Terry for access.
+      </div>
+    </section>
+  );
+};
+
+export default Verify;
--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import Navbar from "./Navbar";
+import Verify from "@/app/verify/page";
 
 const LayoutWrapper = async ({ children }: { children: React.ReactNode }) => {
   const supabase = createClient();
@@ -8,11 +9,20 @@ const LayoutWrapper = async ({ children }: { children: React.ReactNode }) => {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const { data, error } = await supabase
+    .from("agents")
+    .select()
+    .eq("agent_id", user?.id);
+
   return user ? (
-    <div className="flex-1 w-full flex flex-col gap-10 items-center">
-      <Navbar />
-      <div className="flex-1 w-full flex flex-col px-3">{children}</div>
-    </div>
+    data!.length === 0 ? (
+      <Verify />
+    ) : (
+      <div className="flex-1 w-full flex flex-col gap-10 items-center">
+        <Navbar />
+        <div className="flex-1 w-full flex flex-col px-3">{children}</div>
+      </div>
+    )
   ) : (
     <>{children}</>
   );
